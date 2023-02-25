64dd79d49e1d5fd50fdf55ab1b3ac288a5a3ddd4
LadyMisfortunate <80325540+LadyMisfortunate@users.noreply.github.com>
2023-02-25 13:13:32 +0100

    add sample javascript interop

---
+++ BlazorJavascript/JavascriptComponent.Sample/ChartInterop.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavascriptComponent.Sample
+{
+    public class ChartInterop : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public ChartInterop(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+              "import", "./_content/JavascriptComponent.Sample/chartScript.js").AsTask());
+        }
+
+        public async ValueTask ShowChart()
+        {
+            var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("showChart");
+        }
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ BlazorJavascript/JavascriptComponent.Sample/wwwroot/chartScript.js
@@ -0,0 +1,56 @@
+﻿//I took some code  from example in https://www.chartjs.org/docs/latest/getting-started/
+
+//Loading Javascript Library ìn Component, when we use component dont need to import any javascript in main project
+//but you can load javascript in main project like <script scr="..."></script> as well
+
+function LoadChartJS() {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/chart.js';
+    script.type = "text/javascript";
+    script.onload = () => createChart();
+    document.head.appendChild(script);
+}
+
+export function showChart() {
+    LoadChartJS()
+}
+
+function createChart() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
